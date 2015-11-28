38e5aeee9632a18d83f7ca814caf322e14831e9c
Justin Commu <Justin.Commu@loblaw.ca>
2015-11-28 15:59:44 -0500

    Made the library public friendly :) 1. You can now show debug/data msgs to your scenarios. 2. You can now show ENV vars in your main header.

--- lib/ugly_face.rb
+++ lib/ugly_face.rb
@@ -2,47 +2,26 @@ require "ugly_face/version"
 require "ugly_face/formatter/html"
 
 module UglyFace
-  @index = 0
-  @scenario_msgs = Hash.new
-  @msg_storage = Array.new
-  @cli_output=''
-  @html_output=''
 
-  def self.add_msg(key='', msg='')
-    hash_data=''
-    msg.each { |k, v| hash_data << "#{k}: #{v}+ " } if msg.is_a?(Hash)
-    @scenario_msgs[:"#{key}"] = "#{(hash_data == '') ? msg : hash_data}"
+  @msg_storage= Array.new
+
+  def self.set_header(data)
+    data_2= Hash.new
+    data.each { |k, v| (data_2.length >= data.length) ? break : data_2[:"#{k}"]= data.delete(k) }
+    @header_row_1= data
+    @header_row_2= data_2
   end
 
-
-  def self.process_scenario
-    raw_output=''
-    @scenario_msgs.each { |key, value| raw_output << "+#{key}:+ #{value}+" }
-    raw_output= raw_output.gsub("+ ++","++")
-    @cli_output= raw_output.gsub("+","\n")
-    @html_output= raw_output.gsub("+","<br />")
-    @msg_storage.push(@html_output)
-    $stdout.puts @cli_output unless ENV['DEBUG']== 'FALSE'
+  def self.get_header(row)
+    (row == 1) ? @header_row_1 : @header_row_2
   end
 
-  def self.reset
-    @scenario_msgs= {}
-    @cli_output= ''
-    @html_output= ''
+  def self.log_scenario_msg(data)
+    @msg_storage.push(data)
   end
 
-  def self.pop_scenario
-    @index+=1 #Do this first so last line (returned data) is the array readout.
-    @msg_storage.at(@index-1)
+  def self.fetch_scenario_msg
+    @msg_storage.shift
   end
-
-  def self.formatted_cli
-    @cli_output
-  end
-
-  def self.formatted_html
-    @html_output
-  end
-
 
 end
